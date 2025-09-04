fd39ce42f8c7aea8d52f94baa699c7234860e04a
stevihiene-git <122815958+stevihiene-git@users.noreply.github.com>
2025-09-04 10:38:12 +0100

    Update auth.py

--- note_app/auth.py
+++ note_app/auth.py
@@ -59,27 +59,11 @@ def signup():
     current_date = date.strftime("%Y")
     return render_template("signup.html", title="Sign Up", form=form, current_date=current_date)
 
-# @auth2.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('views.home'))
-    
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user and check_password_hash(user.password_hash, form.password.data):
-#             login_user(user)
-#             next_page = request.args.get('next')
-#             return redirect(next_page) if next_page else redirect(url_for('views.home'))
-#         else:
-#             flash('Login unsuccessful. Please check email and password.', 'danger')
-    
-#     date = datetime.now()
-#     current_date = date.strftime("%Y")
-#     return render_template("login.html", title="Login", form=form, current_date=current_date)
+    return render_template("login.html", title="Login", form=form, current_date=current_date)
 
 @auth2.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('views.home'))
+
 
