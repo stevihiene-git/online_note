e52856d4f091cf642efaf82a8ea6536f87436e34
stevihiene-git <122815958+stevihiene-git@users.noreply.github.com>
2025-09-05 22:35:23 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,12 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=5000)
+
+
+# # wsgi.py
+# from note_app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(debug=False, host="0.0.0.0", port=5000)
